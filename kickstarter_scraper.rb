18643a45f0d07c13221d26fe462cb0e7272be733
axton21 <axton_n@yahoo.com>
2015-02-11 18:21:18 -0500

    reindent

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,22 +10,22 @@ require 'pry-nav'
   #location: project.css("ul.project-meta").text
   #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
+  def create_project_hash
+    html = File.read('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
 
-  projects = {}
-  
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title] ={
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
+    projects = {}
+
+    kickstarter.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title] ={
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+        :description => project.css("p.bbcard_blurb").text,
+        :location => project.css("ul.project-meta").text,
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      }
   # binding.pry
-end
-projects
-end
+    end
+    projects
+  end
 # create_project_hash
